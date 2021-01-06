742db412c8ba8b4adb44a73957c387c3cbfabb86
Etoneja <mikhail.zanozin@gmail.com>
2021-01-07 00:59:27 +0300

    create base form

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container py-4">
+    <div class="row">
+      <div class="col-12">
+        <form v-on:submit.prevent="onSubmit">
+          <div class="form-group">
+            <label>First Name:</label>
+            <input v-model="form.firstName" type="text" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label>Last Name:</label>
+            <input v-model="form.lastName" type="text" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label>Email:</label>
+            <input v-model="form.email" type="email" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <button 
+              v-bind:disabled="!formIsValid"
+              type="submit" 
+              class="btn btn-primary"
+            >Submit</button>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        email: ""
+      }
+    }
+  },
+  computed: {
+    formIsValid: function() {
+      return (
+        this.form.firstName.length > 0 &&
+        this.form.lastName.length > 0 &&
+        this.form.email.length > 0
+      );
+    }
+  },
+  methods: {
+    onSubmit: function() {
+      if (!this.formIsValid) return
+      console.log("form submit")
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
